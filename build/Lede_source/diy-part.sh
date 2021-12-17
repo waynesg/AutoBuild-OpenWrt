3ee3de1e97b64ea7d30b1cef1ba20844c51e6b52
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2021-12-17 22:44:46 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -47,7 +47,7 @@ sed -i 's/"实时流量监测"/"流量"/g' `grep "实时流量监测" -rl ./`
 sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
 sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./`
 sed -i 's/"在线用户"/"在线设备"/g' package/waynesg/luci-app-onliner/luasrc/controller/onliner.lua
-sed -i 's/"autoipsetadder"/"添加IP"/g' package/waynesg/luci-app-autoipsetadder/luasrc/controller/autoipsetadder.lua
+sed -i 's/"autoipsetadder"/"自动设置IP"/g' `grep "autoipsetadder" -rl ./`
 
 
 #services menu
