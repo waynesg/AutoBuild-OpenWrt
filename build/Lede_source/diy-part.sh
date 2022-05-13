ee5d8644bf9b9cb2f83f5c9eb9b22c8386f44339
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2022-05-13 11:12:53 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -95,6 +95,7 @@ TIME b "插件 重命名..."
 echo "重命名系统菜单"
 #system menu
 sed -i 's/"Web 管理"/"Web管理"/g' `grep "Web 管理" -rl ./`
+sed -i 's/"备份\/升级"/"备份升级"/g' `grep "备份\/升级" -rl ./`
 sed -i 's/"管理权"/"权限管理"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i 's/"重启"/"立即重启"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 #sed -i 's/"系统"/"系统设置"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
