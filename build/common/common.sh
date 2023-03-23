c8d5703a73fb60ca4ede6fb3ada0b58753a2edbb
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-03-23 23:58:41 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -44,13 +44,11 @@ find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-
 
 
 ################################################################################################################
-# 天灵源码18.06 diy.sh文件
+# 官方22.03 diy.sh文件
 ################################################################################################################
-Diy_mortal() {
+Diy_Official() {
 
-find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-light' | xargs -i rm -rf {}
 find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-openwrt' -o -name 'luci-app-cifs' | xargs -i rm -rf {}
-find . -name 'luci-app-wrtbwmon' -o -name 'wrtbwmon' | xargs -i rm -rf {}
 }
 
 
@@ -73,7 +71,7 @@ elif [[ "${REPO_BRANCH}" == "19.07" ]]; then
 	cp -Rf "${Home}"/build/common/LIENOL/files "${Home}"
 	cp -Rf "${Home}"/build/common/LIENOL/diy/* "${Home}"
 	cp -Rf "${Home}"/build/common/LIENOL/patches/* "${PATH1}/patches"
-elif [[ "${REPO_BRANCH}" == "openwrt-18.06" ]]; then
+elif [[ "${REPO_BRANCH}" == "openwrt-22.03" ]]; then
 	cp -Rf "${Home}"/build/common/MORTAL/files "${Home}"
 	cp -Rf "${Home}"/build/common/MORTAL/diy/* "${Home}"
 	cp -Rf "${Home}"/build/common/MORTAL/patches/* "${PATH1}/patches"
