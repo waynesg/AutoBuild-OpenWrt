b296c8f1fed91368dd1ac8d69846e23a43f5a2fa
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-03-29 10:46:49 +0800

    Update diy-part.sh

--- build/Official_source/diy-part.sh
+++ build/Official_source/diy-part.sh
@@ -1,1 +1,10 @@
-
+cat >"$CLEAR_PATH" <<-EOF
+packages
+config.buildinfo
+feeds.buildinfo
+openwrt-x86-64-generic-kernel.bin
+openwrt-x86-64-generic.manifest
+openwrt-x86-64-generic-squashfs-rootfs.img.gz
+sha256sums
+version.buildinfo
+EOF
