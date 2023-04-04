33f1b8bb7e3db3c1d71099ac61b8e9185d8c5279
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-04-04 09:57:33 +0800

    Update 07_fw3.sh

--- build/Official/SCRIPTS/07_fw3.sh
+++ build/Official/SCRIPTS/07_fw3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 滚回fw3
-sed -i 's,iptables-nft,iptables-legacy,g' ./package/new/luci-app-passwall2/Makefile
-sed -i 's,iptables-nft,iptables-legacy,g' ./package/new/luci-app-passwall/Makefile
-sed -i 's,iptables-nft +kmod-nft-fullcone,iptables-mod-fullconenat,g' ./package/new/addition-trans-zh/Makefile
+sed -i 's,iptables-nft,iptables-legacy,g' ./package/waynesg/luci-app-passwall2/Makefile
+sed -i 's,iptables-nft,iptables-legacy,g' ./package/waynesg/luci-app-passwall/Makefile
+sed -i 's,iptables-nft +kmod-nft-fullcone,iptables-mod-fullconenat,g' ./package/waynesg/addition-trans-zh/Makefile
 rm -rf ./feeds/packages/net/miniupnpd
 cp -rf ../lede_pkg/net/miniupnpd ./feeds/packages/net/miniupnpd
 rm -rf ./feeds/luci/applications/luci-app-upnp
@@ -24,11 +24,11 @@ CONFIG_PACKAGE_dnsmasq_full_nftset=n
 ' >>./.config
 rm -rf ./feeds/luci/applications/luci-app-zerotier
 cp -rf ../lede_luci/applications/luci-app-zerotier ./feeds/luci/applications/luci-app-zerotier
-wget -P feeds/luci/applications/luci-app-zerotier/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/move_2_services.sh
-chmod -R 755 ./feeds/luci/applications/luci-app-zerotier/move_2_services.sh
-pushd feeds/luci/applications/luci-app-zerotier
-bash move_2_services.sh
-popd
-ln -sf ../../../feeds/luci/applications/luci-app-zerotier ./package/feeds/luci/luci-app-zerotier
+#wget -P feeds/luci/applications/luci-app-zerotier/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/move_2_services.sh
+#chmod -R 755 ./feeds/luci/applications/luci-app-zerotier/move_2_services.sh
+#pushd feeds/luci/applications/luci-app-zerotier
+#bash move_2_services.sh
+#popd
+#ln -sf ../../../feeds/luci/applications/luci-app-zerotier ./package/feeds/luci/luci-app-zerotier
 
 exit 0
