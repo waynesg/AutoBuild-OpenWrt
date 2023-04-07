7c35121dc3d743388c9b95291c6f6ec3f6be3fe5
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-04-07 18:53:34 +0800

    Create common.sh

---
+++ build/Immortalwrt_source/common.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+# https://github.com/mdtycl/Compile-OpenWrt
+# common Module by mdtycl
+# matrix.target=${Modelfile}
+
+TIME() {
+Compte=$(date +%Y年%m月%d号%H时%M分)
+[[ -z "$1" ]] && {
+	echo -ne " "
+} || {
+     case $1 in
+	r) export Color="\e[31m";;
+	g) export Color="\e[32m";;
+	b) export Color="\e[34m";;
+	y) export Color="\e[33m";;
+	z) export Color="\e[35m";;
+	l) export Color="\e[36m";;
+      esac
+	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+	 }
+      }
+}
+Diy_Immortalwrt() {
+
+#find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-openwrt' -o -name 'luci-app-cifs' | xargs -i rm -rf {}
+
+if [[ ! "${Modelfile}" == "openwrt_amlogic" ]]; then
+	sed -i '/IMAGES_GZIP/d' "${PATH1}/${CONFIG_FILE}" > /dev/null 2>&1
+	echo -e "\nCONFIG_TARGET_IMAGES_GZIP=y" >> "${PATH1}/${CONFIG_FILE}"
+fi
+
+}
+
+################################################################################################################
+# Official 22.03 diy.sh文件
+################################################################################################################
+Diy_Official() {
+
+#find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-openwrt' -o -name 'luci-app-cifs' | xargs -i rm -rf {}
+
+if [[ ! "${Modelfile}" == "openwrt_amlogic" ]]; then
+	sed -i '/IMAGES_GZIP/d' "${PATH1}/${CONFIG_FILE}" > /dev/null 2>&1
+	echo -e "\nCONFIG_TARGET_IMAGES_GZIP=y" >> "${PATH1}/${CONFIG_FILE}"
+fi
+
+}
+
+
+################################################################################################################
+# 全部作者源码公共diy.sh文件
+################################################################################################################
+Diy_all() {
+
+if [[ ${REGULAR_UPDATE} == "true" ]]; then
+        git clone https://github.com/waynesg/luci-app-autoupdate feeds/luci/applications/luci-app-autoupdate
+	cp -Rf "${PATH1}"/{AutoUpdate.sh,AutoBuild_Tools.sh,replace.sh} package/base-files/files/bin
+	sed -i 's/"定时更新"/"更新固件"/g' feeds/luci/applications/luci-app-autoupdate/po/zh-cn/autoupdate.po
+	sed -i 's/定时更新 LUCI/固件更新 LUCI/g' feeds/luci/applications/luci-app-autoupdate/po/zh-cn/autoupdate.po
+fi
+if [ -n "$(ls -A "${PATH1}/diy" 2>/dev/null)" ]; then
+	cp -Rf "${PATH1}"/diy/* "${Home}"
+fi
+if [ -n "$(ls -A "${PATH1}/files" 2>/dev/null)" ]; then
+	cp -Rf "${PATH1}/files" "${Home}" && chmod -R +x ${Home}/files
+fi
+if [ -n "$(ls -A "${PATH1}/patches" 2>/dev/null)" ]; then
+	find "${PATH1}/patches" -type f -name '*.patch' -print0 | sort -z | xargs -I % -t -0 -n 1 sh -c "cat '%'  | patch -d './' -p1 --forward --no-backup-if-mismatch"
+fi
+}
+################################################################################################################
+# 为编译做最后处理
+################################################################################################################
+Diy_chuli() {
+
+if [[ "${BY_INFORMATION}" == "true" ]]; then
+	grep -i CONFIG_PACKAGE_luci-app .config | grep  -v \# > Plug-in
+	grep -i CONFIG_PACKAGE_luci-theme .config | grep  -v \# >> Plug-in
+	if [[ `grep -c "CONFIG_PACKAGE_luci-i18n-qbittorrent-zh-cn=y" ${Home}/.config` -eq '0' ]]; then
+		if [[ `grep -c "luci-app-qbittorrent_static" ${Home}/Plug-in` -eq '1' ]]; then
+			sed -i '/qbittorrent/d' Plug-in
+		fi
+	fi
+	sed -i '/INCLUDE/d' Plug-in > /dev/null 2>&1
+	sed -i '/=m/d' Plug-in > /dev/null 2>&1
+	sed -i 's/CONFIG_PACKAGE_/、/g' Plug-in
+	sed -i 's/=y/\"/g' Plug-in
+	awk '$0=NR$0' Plug-in > Plug-2
+	awk '{print "	" $0}' Plug-2 > Plug-in
+	sed -i "s/^/TIME g \"/g" Plug-in
+	cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq -c > CPU
+        cat /proc/cpuinfo | grep "cpu cores" | uniq >> CPU
+        sed -i 's|[[:space:]]||g; s|^.||' CPU && sed -i 's|CPU||g; s|pucores:||' CPU
+        CPUNAME="$(awk 'NR==1' CPU)" && CPUCORES="$(awk 'NR==2' CPU)"
+        rm -rf CPU
+fi
+rm -rf ${Home}/files/{README,README.md}
+}
+
+################################################################################################################
+# 编译信息
+################################################################################################################
+Diy_xinxi() {
+if [[ "${TARGET_PROFILE}" =~ (friendlyarm_nanopi-r2s|friendlyarm_nanopi-r4s|armvirt) ]]; then
+	REGULAR_UPDATE="false"
+fi
+echo
+TIME b "编译源码: ${CODE}"
+TIME b "源码链接: ${REPO_URL}"
+TIME b "源码分支: ${REPO_BRANCH}"
+TIME b "源码作者: ${ZUOZHE}"
+TIME b "固件内核: ${PATCHVER}"
+TIME b "Luci版本: ${OpenWrt_name}"
+[[ "${Modelfile}" == "openwrt_amlogic" ]] && {
+	TIME b "编译机型: ${TARGET_model}"
+	TIME b "打包内核: ${TARGET_kernel}"
+} || {
+	TIME b "编译机型: ${TARGET_PROFILE}"
+}
+TIME b "固件作者: ${Author}"
+TIME b "仓库地址: ${Github}"
+TIME b "启动编号: #${Run_number}（${CangKu}仓库第${Run_number}次启动[${Run_workflow}]工作流程）"
+TIME b "编译时间: ${Compte}"
+[[ "${Modelfile}" == "openwrt_amlogic" ]] && {
+	TIME g "友情提示：您当前使用【${Modelfile}】文件夹编译【${TARGET_model}】固件"
+} || {
+	TIME g "友情提示：您当前使用【${Modelfile}】文件夹编译【${TARGET_PROFILE}】固件"
+}
+echo
+
+if [[ ${UPLOAD_FIRMWARE} == "true" ]]; then
+	TIME y "上传固件在github actions: 开启"
+else
+	TIME r "上传固件在github actions: 关闭"
+fi
+if [[ ${UPLOAD_CONFIG} == "true" ]]; then
+	TIME y "上传[.config]配置文件: 开启"
+else
+	TIME r "上传[.config]配置文件: 关闭"
+fi
+if [[ ${UPLOAD_BIN_DIR} == "true" ]]; then
+	TIME y "上传BIN文件夹(固件+IPK): 开启"
+else
+	TIME r "上传BIN文件夹(固件+IPK): 关闭"
+fi
+if [[ ${UPLOAD_COWTRANSFER} == "true" ]]; then
+	TIME y "上传固件至【奶牛快传】和【WETRANSFER】: 开启"
+else
+	TIME r "上传固件至【奶牛快传】和【WETRANSFER】: 关闭"
+fi
+if [[ ${UPLOAD_RELEASE} == "true" ]]; then
+	TIME y "发布固件: 开启"
+else
+	TIME r "发布固件: 关闭"
+fi
+if [[ ${SERVERCHAN_SCKEY} == "true" ]]; then
+	TIME y "微信/电报通知: 开启"
+else
+	TIME r "微信/电报通知: 关闭"
+fi
+if [[ ${SSH_ACTIONS} == "true" ]]; then
+	TIME y "SSH远程连接: 开启"
+else
+	TIME r "SSH远程连接: 关闭"
+fi
+if [[ ${BY_INFORMATION} == "true" ]]; then
+	TIME y "编译信息显示: 开启"
+fi
+if [[ ${SSHYC} == "true" ]]; then
+	TIME y "SSH远程连接临时开关: 开启"
+fi
+if [[ ${REGULAR_UPDATE} == "true" ]]; then
+	TIME y "把定时自动更新插件编译进固件: 开启"
+else
+	TIME r "把定时自动更新插件编译进固件: 关闭"
+fi
+if [[ ${REGULAR_UPDATE} == "true" ]]; then
+	echo
+	TIME l "定时自动更新信息"
+	TIME z "插件版本: ${AutoUpdate_Version}"
+	if [[ ${TARGET_PROFILE} == "x86-64" ]]; then
+		TIME b "传统固件: ${Legacy_Firmware}"
+		TIME b "UEFI固件: ${UEFI_Firmware}"
+		TIME b "固件后缀: ${Firmware_sfx}"
+	else
+		TIME b "固件名称: ${Up_Firmware}"
+		TIME b "固件后缀: ${Firmware_sfx}"
+	fi
+	TIME b "固件版本: ${Openwrt_Version}"
+	TIME b "云端路径: ${Github_UP_RELEASE}"
+	TIME g "《编译成功后，会自动把固件发布到指定地址，然后才会生成云端路径》"
+	TIME g "《普通的那个发布固件跟云端的发布路径是两码事，如果你不需要普通发布的可以不用打开发布功能》"	
+else
+	echo
+fi
+echo
+TIME z " 系统空间      类型   容量  已用  可用 使用率"
+cd ../ && df -hT $PWD && cd openwrt
+echo
+TIME z "  本编译 服务器的 CPU型号为 [ ${CPUNAME} ]"
+echo
+TIME z "  使用 核心数 为 [ ${CPUCORES} ], 线程数为 [ $(nproc) ]"
+echo
+TIME z "  随机分配到 E5系列CPU 编译是 最慢的, 8171M 的CPU 快很多，8272CL 的又比 8171M 快些！"
+echo
+TIME z "  如果编译的插件较多，而又分配到 E5系列 的 CPU，建议关闭 重新再来！"
+echo
+TIME z "  下面将使用 [ $(nproc) 线程 ] 编译固件"
+echo
+if [ -n "$(ls -A "${Home}/EXT4" 2>/dev/null)" ]; then
+	echo
+	echo
+	chmod -R +x ${Home}/EXT4
+	source ${Home}/EXT4
+	rm -rf EXT4
+fi
+if [ -n "$(ls -A "${Home}/Chajianlibiao" 2>/dev/null)" ]; then
+	echo
+	echo
+	chmod -R +x ${Home}/CHONGTU
+	source ${Home}/CHONGTU
+	rm -rf {CHONGTU,Chajianlibiao}
+	echo
+	echo
+fi
+if [ -n "$(ls -A "${Home}/Plug-in" 2>/dev/null)" ]; then
+	TIME r "	      已选插件列表"
+	chmod -R +x ${Home}/Plug-in
+	source ${Home}/Plug-in
+	rm -rf {Plug-in,Plug-2}
+	echo
+fi
+}
