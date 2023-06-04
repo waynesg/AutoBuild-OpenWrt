29458dc5ffc56f9a528044d78f72e969b4e9304e
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-06-04 20:12:00 +0800

    Update preset-clash-core.sh

--- build/Lede_source/waynesg/preset-clash-core.sh
+++ build/Lede_source/waynesg/preset-clash-core.sh
@@ -12,8 +12,9 @@
 mkdir -p files/etc/openclash/core
 mkdir -p files/usr/share/openclash/ui
 
+CORE_VERSION="$(curl -fsSL https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version | grep '^[0-9].*')"
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}')
+CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-${1}-${CORE_VERSION}.gz"
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
