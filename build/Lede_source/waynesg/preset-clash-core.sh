0cd85dd10cf1fd58d5da112f2eeafda6e97268f9
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-06-11 16:37:54 +0800

    Update preset-clash-core.sh

--- build/Lede_source/waynesg/preset-clash-core.sh
+++ build/Lede_source/waynesg/preset-clash-core.sh
@@ -10,7 +10,7 @@
 #=================================================
 
 mkdir -p files/etc/openclash/core
-mkdir -p files/usr/share/openclash/ui
+#mkdir -p files/usr/share/openclash/ui
 
 CORE_VERSION="$(curl -fsSL https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version | grep '^[0-9].*')"
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
@@ -18,7 +18,7 @@ CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-YACD_META_URL="https://raw.githubusercontent.com/DustinWin/clash-tools/main/Yacd-meta/Yacd-meta.tar.gz"
+#YACD_META_URL="https://raw.githubusercontent.com/DustinWin/clash-tools/main/Yacd-meta/Yacd-meta.tar.gz"
 
 wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
 wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
@@ -29,4 +29,4 @@ wget -qO- YACD_META_URL > tar xOvz > files/usr/share/openclash/ui/yacd
 
 
 chmod +x files/etc/openclash/core/clash*
-chmod +x files/usr/share/openclash/ui/yacd
+#chmod +x files/usr/share/openclash/ui/yacd
