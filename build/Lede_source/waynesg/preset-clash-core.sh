af21055f56bef4cf5acc33521137c65011e46e97
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-09-05 23:45:39 +0800

    Update preset-clash-core.sh

--- build/Lede_source/waynesg/preset-clash-core.sh
+++ build/Lede_source/waynesg/preset-clash-core.sh
@@ -11,6 +11,7 @@
 
 mkdir -p files/etc/openclash/core
 mkdir -p files/usr/share/openclash/ui/yacd
+mkdir -p files/usr/share/openclash/ui/dashboard
 
 CORE_VERSION="$(curl -fsSL https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version | grep '^[0-9].*')"
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
@@ -18,7 +19,11 @@ CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-YACD_META_URL="https://raw.githubusercontent.com/DustinWin/clash-tools/main/Yacd-meta/Yacd-meta.tar.gz"
+YACD_META_URL="https://raw.githubusercontent.com/DustinWin/clash-tools/main/Dashboard/Yacd-meta.tar.gz"
+RAZORD_META_URL="https://raw.githubusercontent.com/DustinWin/clash-tools/main/Dashboard/Razord-meta.tar.gz"
+#YACD_URL="https://raw.githubusercontent.com/DustinWin/clash-tools/main/Dashboard/yacd.tar.gz"
+#DASHBOARD_URL="https://raw.githubusercontent.com/DustinWin/clash-tools/main/Dashboard/clash-dashboard.tar.gz"
+               
 
 wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
 wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
@@ -26,6 +31,7 @@ wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 wget -qO- $YACD_META_URL | tar xvz -C files/usr/share/openclash/ui/yacd
+wget -qO- $RAZORD_META_URL | tar xvz -C files/usr/share/openclash/ui/dashboard
 
 
 chmod +x files/etc/openclash/core/clash*
