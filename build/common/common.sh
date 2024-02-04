d52d894a2b19774eac6f3f2529613a979e3020ca
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-02-04 14:54:16 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -376,24 +376,6 @@ echo
 make defconfig > /dev/null 2>&1
 echo "TIME b \"					插件冲突信息\"" > ${Home}/CHONGTU
 
-if [[ `grep -c "CONFIG_TARGET_ROOTFS_EXT4FS=y" ${Home}/.config` -eq '1' ]]; then
-	if [[ `grep -c "CONFIG_TARGET_ROOTFS_PARTSIZE" ${Home}/.config` -eq '0' ]]; then
-		sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/d' ${Home}/.config > /dev/null 2>&1
-		echo -e "\nCONFIG_TARGET_ROOTFS_PARTSIZE=950" >> ${Home}/.config
-	fi
-	egrep -o "CONFIG_TARGET_ROOTFS_PARTSIZE=+.*?[0-9]" ${Home}/.config > ${Home}/EXT4PARTSIZE
-	sed -i 's|CONFIG_TARGET_ROOTFS_PARTSIZE=||g' ${Home}/EXT4PARTSIZE
-	PARTSIZE="$(cat EXT4PARTSIZE)"
-	if [[ "${PARTSIZE}" -lt "950" ]];then
-		sed -i '/CONFIG_TARGET_ROOTFS_PARTSIZE/d' ${Home}/.config > /dev/null 2>&1
-		echo -e "\nCONFIG_TARGET_ROOTFS_PARTSIZE=950" >> ${Home}/.config
-		echo "TIME g \" \"" > ${Home}/EXT4
-		echo "TIME r \"EXT4提示：请注意，您选择了ext4安装的固件格式,而检测到您的分配的固件系统分区过小\"" >> ${Home}/EXT4
-		echo "TIME y \"为避免编译出错,建议修改成950或者以上比较好,已帮您修改成950M\"" >> ${Home}/EXT4
-		echo "TIME g \" \"" >> ${Home}/EXT4
-	fi
-	rm -rf ${Home}/EXT4PARTSIZE
-fi
 if [ -n "$(ls -A "${Home}/Chajianlibiao" 2>/dev/null)" ]; then
 	echo "TIME y \"  插件冲突会导致编译失败，以上操作如非您所需，请关闭此次编译，重新开始编译，避开冲突重新选择插件\"" >>CHONGTU
 	echo "TIME z \"\"" >>CHONGTU
