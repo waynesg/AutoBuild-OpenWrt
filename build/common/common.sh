6b753badf54747df64d987bac398e317074836b5
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-02-05 23:14:38 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -187,7 +187,7 @@ find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-
 #find . -name 'netdata' -o -name 'luci-app-netdata' -o -name 'luci-app-serverchan' | xargs -i rm -rf {}
 #find . -name 'mosdns' -o -name 'luci-app-mosdns' -o -name 'v2dat' | xargs -i rm -rf {}
 find . -name 'adguardhome' -o -name 'go-aliyundrive-webdav' | xargs -i rm -rf {}
-find . -name 'luci-app-unblockmusic' -o -name 'luci-app-pushbot' -o -name 'msd_lite' -o -name 'pdnsd-alt' -o -name 'v2ray-geodata' | xargs -i rm -rf {}
+find . -name 'luci-app-qbittorrent' -o -name 'luci-app-unblockmusic' -o -name 'luci-app-pushbot' -o -name 'msd_lite' -o -name 'pdnsd-alt' -o -name 'v2ray-geodata' | xargs -i rm -rf {}
 }
 ################################################################################################################
 # LIENOL源码通用diy.sh文件
