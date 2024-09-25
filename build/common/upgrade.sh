f4b235985839d8a5d71f77b885e1fa19c9ad4a50
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-09-25 18:56:12 +0800

    Update upgrade.sh

--- build/common/upgrade.sh
+++ build/common/upgrade.sh
@@ -120,6 +120,7 @@ GET_TARGET_INFO() {
 
 Diy_Part1() {
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += luci-app-autoupdate luci-app-ttyd/g' target/linux/*/Makefile
+sed -i 's/+libpcre \\$/+libpcre2 \\/g' package/feeds/telephony/freeswitch/Makefile
 }
 
 Diy_Part2() {
