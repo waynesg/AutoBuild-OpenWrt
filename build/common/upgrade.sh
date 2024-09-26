f850e4e0b4ff51b6d2fd5aff9a24b27425f0bf4c
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-09-26 19:19:24 +0800

    Update upgrade.sh

--- build/common/upgrade.sh
+++ build/common/upgrade.sh
@@ -9,59 +9,42 @@ GET_TARGET_INFO() {
 	}
 	case "${REPO_BRANCH}" in
 	"master")
-            if [[ "${FOLDER_NAME}" == "Lede_source" ]]; then
-		LUCI_Name="18.06"
+                LUCI_Name="18.06"
 		REPO_Name="lede"
 		ZUOZHE="Lean's"
 		if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
 			Legacy_Firmware="openwrt-x86-64-generic-squashfs-combined.${Firmware_sfxo}"
 			UEFI_Firmware="openwrt-x86-64-generic-squashfs-combined-efi.${Firmware_sfxo}"
 			Firmware_sfx="${Firmware_sfxo}"
-		# elif [[ "${TARGET_PROFILE}" == "phicomm_k3" ]]; then
-		# 	Up_Firmware="openwrt-bcm53xx-generic-${TARGET_PROFILE}-squashfs.trx"
-		# 	Firmware_sfx="trx"
-		# elif [[ "${TARGET_PROFILE}" == "xiaomi_mi-router-3g" ]]; then
-		# 	TARGET_PROFILE="xiaomi_mir3g"
-		# 	Up_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
-		# 	Firmware_sfx="bin"
-		# elif [[ "${TARGET_PROFILE}" == "xiaomi_mi-router-3g-v2" ]]; then
-		# 	TARGET_PROFILE="xiaomi_mir3gv2"
-		# 	Up_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
-		# 	Firmware_sfx="bin"
+		elif [[ "${TARGET_PROFILE}" == "phicomm_k3" ]]; then
+			Up_Firmware="openwrt-bcm53xx-generic-${TARGET_PROFILE}-squashfs.trx"
+			Firmware_sfx="trx"
+		elif [[ "${TARGET_PROFILE}" == "xiaomi_mi-router-3g" ]]; then
+			TARGET_PROFILE="xiaomi_mir3g"
+			Up_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
+			Firmware_sfx="bin"
+		elif [[ "${TARGET_PROFILE}" == "xiaomi_mi-router-3g-v2" ]]; then
+			TARGET_PROFILE="xiaomi_mir3gv2"
+			Up_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
+			Firmware_sfx="bin"
 		else
 			Up_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
 			Firmware_sfx="bin"
 		fi
-            elif [[ "${FOLDER_NAME}" == "Immortalwrt_source" ]]; then
-	# ImmortalWrt 源相关设置
-	        LUCI_Name="immortalwrt-23.05"
-		REPO_Name="immortalwrt"
-		ZUOZHE="immortalwrt"
+	;;
+	"openwrt-23.05")
+		LUCI_Name="23.05"
+		REPO_Name="Immortalwrt"
+		ZUOZHE="Immortalwrt's"
 		if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
 			Legacy_Firmware="immortalwrt-x86-64-generic-squashfs-combined.${Firmware_sfxo}"
-			UEFI_Firmware="immortalwrt-x86-64-generic-squashfs-combined-efi.${Firmware_sfxo}"				Firmware_sfx="${Firmware_sfxo}"
+			UEFI_Firmware="immortalwrt-x86-64-generic-squashfs-combined-efi.${Firmware_sfxo}"				
+                        Firmware_sfx="${Firmware_sfxo}"
 		elif [[ "${TARGET_PROFILE}" == "phicomm-k3" ]]; then
 			Up_Firmware="immortalwrt-bcm53xx-${TARGET_PROFILE}-squashfs.trx"
 			Firmware_sfx="trx"
 		else
 			Up_Firmware="immortalwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
-			Firmware_sfx="bin"
-		fi
-	fi
-	;;
-	"19.07")
-		LUCI_Name="19.07"
-		REPO_Name="lienol"
-		ZUOZHE="Lienol's"
-		if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
-			Legacy_Firmware="openwrt-x86-64-combined-squashfs.${Firmware_sfxo}"
-			UEFI_Firmware="openwrt-x86-64-combined-squashfs-efi.${Firmware_sfxo}"
-			Firmware_sfx="${Firmware_sfxo}"
-		elif [[ "${TARGET_PROFILE}" == "phicomm-k3" ]]; then
-			Up_Firmware="openwrt-bcm53xx-${TARGET_PROFILE}-squashfs.trx"
-			Firmware_sfx="trx"
-		else
-			Up_Firmware="openwrt-${TARGET_BOARD}-${TARGET_SUBTARGET}-${TARGET_PROFILE}-squashfs-sysupgrade.bin"
 			Firmware_sfx="bin"
 		fi
 	;;
