e88c1d9a5c1fdea81e3d037caebe65769cff8990
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-10-09 22:36:19 +0800

    Update diy-repo-script.sh

--- build/Immortalwrt_source/waynesg/diy-repo-script.sh
+++ build/Immortalwrt_source/waynesg/diy-repo-script.sh
@@ -92,4 +92,6 @@ git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git package/wayn
 git clone --depth=1 https://github.com/gSpotx2f/luci-app-cpu-status package/waynesg/luci-app-cpu-status
 #tn-netports
 git clone --depth=1 https://github.com/muink/luci-app-tn-netports package/waynesg/luci-app-tn-netports
+#turboacc
+git clone --depth=1 https://github.com/chenmozhijin/turboacc.git package/waynesg/luci-app-turboacc
 
