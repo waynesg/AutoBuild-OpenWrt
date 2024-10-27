b0082725075e599413bbc50c49e8f368b08a44f4
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-10-27 11:51:38 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -183,7 +183,7 @@ git push --force "https://${REPO_TOKEN}@github.com/${Apidz}" HEAD:main
 # LEDE源码通用diy.sh文件
 ################################################################################################################
 Diy_lede() {
-find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-theme-material' -o -name 'luci-app-serverchan' | xargs -i rm -rf {}
+find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-theme-material' -name 'smartdns' -o -name 'luci-app-smartdns'  | xargs -i rm -rf {}
 #find . -name 'netdata' -o -name 'luci-app-netdata' | xargs -i rm -rf {}
 find . -name 'mosdns' -o -name 'luci-app-mosdns' -o -name 'luci-app-openclash' -o -name 'v2dat' | xargs -i rm -rf {}
 #find . -name 'adguardhome' -o -name 'go-aliyundrive-webdav' | xargs -i rm -rf {}
