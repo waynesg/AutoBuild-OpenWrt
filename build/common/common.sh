dcac9e7c4051bc7f5f9a6b7b39eb2e3143b3c9a9
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-10-27 11:04:03 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -183,11 +183,11 @@ git push --force "https://${REPO_TOKEN}@github.com/${Apidz}" HEAD:main
 # LEDE源码通用diy.sh文件
 ################################################################################################################
 Diy_lede() {
-find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-theme-material' -o -name 'luci-app-serverchan' | xargs -i rm -rf {}
+#find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-theme-material' -o -name 'luci-app-serverchan' | xargs -i rm -rf {}
 #find . -name 'netdata' -o -name 'luci-app-netdata' -o -name 'luci-app-serverchan' | xargs -i rm -rf {}
-find . -name 'mosdns' -o -name 'luci-app-mosdns' -o -name 'smartdns' -o -name 'luci-app-smartdns' -o -name 'pdnsd-alt' -o -name 'v2ray-geodata' | xargs -i rm -rf {}
-find . -name 'adguardhome' -o -name 'go-aliyundrive-webdav' | xargs -i rm -rf {}
-find . -name 'luci-app-qbittorrent' -o -name 'luci-app-unblockmusic' -o -name 'luci-app-pushbot' -o -name 'v2dat' -o -name 'msd_lite' | xargs -i rm -rf {}
+find . -name 'mosdns' -o -name 'luci-app-mosdns' -o -name 'smartdns' -o -name 'luci-app-smartdns' | xargs -i rm -rf {}
+#find . -name 'adguardhome' -o -name 'go-aliyundrive-webdav' | xargs -i rm -rf {}
+#find . -name 'luci-app-qbittorrent' -o -name 'luci-app-unblockmusic' -o -name 'luci-app-pushbot' -o -name 'msd_lite' | xargs -i rm -rf {}
 }
 ################################################################################################################
 # LIENOL源码通用diy.sh文件
