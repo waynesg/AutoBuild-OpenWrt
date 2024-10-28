6fc895983844131b57cf7fbc3783c8916ef92989
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2024-10-28 09:35:13 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -184,7 +184,7 @@ git push --force "https://${REPO_TOKEN}@github.com/${Apidz}" HEAD:main
 ################################################################################################################
 Diy_lede() {
 find . -name 'luci-app-argon-config' -o -name 'luci-theme-argon' -o -name 'luci-theme-material' | xargs -i rm -rf {}
-find . -name 'mosdns' -o -name 'luci-app-mosdns' -o -name 'luci-app-openclash' -o -name 'v2dat' | xargs -i rm -rf {}
+find . -name 'mosdns' -o -name 'luci-app-mosdns' -o -name 'luci-app-openclash' -o -name 'v2dat' -o -name 'luci-app-statistics' | xargs -i rm -rf {}
 }
 ################################################################################################################
 # LIENOL源码通用diy.sh文件
