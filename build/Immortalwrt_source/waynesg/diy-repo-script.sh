39b7fd731c08f2cf686f8741e0779f34d8160a78
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-02-24 19:50:38 +0800

    Update diy-repo-script.sh

--- build/Immortalwrt_source/waynesg/diy-repo-script.sh
+++ build/Immortalwrt_source/waynesg/diy-repo-script.sh
@@ -91,3 +91,4 @@ git clone --depth=1 https://github.com/muink/luci-app-tn-netports package/waynes
 git clone --depth=1 https://github.com/liudf0716/luci-app-yt-dlp package/waynesg/luci-app-yt-dlp
 #uugame
 git clone --depth=1 https://github.com/kiddin9/luci-app-uugamebooster package/waynesg/luci-app-uugamebooster
+clone_or_update_git_repo https://github.com/kiddin9/openwrt-packages package/waynesg/uugamebooster "" uugamebooster
