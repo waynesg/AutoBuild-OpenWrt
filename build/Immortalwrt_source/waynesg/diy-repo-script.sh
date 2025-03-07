d9f393274f6cdf8b72d75043f623e00cdf6982a8
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-03-07 09:52:31 +0800

    Update diy-repo-script.sh

--- build/Immortalwrt_source/waynesg/diy-repo-script.sh
+++ build/Immortalwrt_source/waynesg/diy-repo-script.sh
@@ -85,6 +85,8 @@ git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/wayn
 git clone --depth=1 https://github.com/gSpotx2f/luci-app-cpu-status package/waynesg/luci-app-cpu-status
 #tn-netports
 git clone --depth=1 https://github.com/muink/luci-app-tn-netports package/waynesg/luci-app-tn-netports
+#tailscale
+git clone --depth=1 https://github.com/BootLoopLover/luci-app-tailscale package/waynesg/luci-app-tailscale
 #lucky
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-lucky package/waynesg/luci-app-lucky
 #yt-dlp
