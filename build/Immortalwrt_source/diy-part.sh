4dfaff559b1b3bcbd5e5900104cab4032198c02b
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-04-09 22:24:01 +0800

    Update diy-part.sh

--- build/Immortalwrt_source/diy-part.sh
+++ build/Immortalwrt_source/diy-part.sh
@@ -188,7 +188,6 @@ echo "重命名服务菜单"
 sed -i 's/"解除网易云音乐播放限制"/"网易音乐"/g' feeds/luci/applications/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 sed -i 's/"Vlmcsd KMS 服务器"/"KMS服务"/g' $(grep "KMS 服务器" -rl ./)
 #sed -i 's/"短视频下载"/"视频下载"/g' package/waynesg/luci-app-yt-dlp/po/zh_Hans/yt-dlp.po
-sed -i 's/"Cloudflare 零信任隧道"/"Cloudflare 隧道"/g' feeds/luci/applications/luci-app-cloudflared/po/zh_Hans/cloudflared.po
 
 
 echo "重命名网络菜单"
@@ -214,9 +213,10 @@ sed -i 's/"FTP 服务器"/"FTP 服务"/g' feeds/luci/applications/luci-app-vsftp
 
 echo "重命名VPN菜单"
 #vpn
+sed -i 's/"Cloudflare 零信任隧道"/"Cloudflare 云隧道"/g' feeds/luci/applications/luci-app-cloudflared/po/zh_Hans/cloudflared.po
 sed -i 's/"ZeroTier"/"ZeroTier虚拟网络"/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
 sed -i 's/"OpenVPN"/"OpenVPN 客户端"/g' feeds/luci/applications/luci-app-openvpn/luasrc/controller/openvpn.lua
-sed -i 's/"IPSec VPN Server"/"IPSec VPN 服务器"/g' feeds/luci/applications/luci-app-ipsec-vpnd/root/usr/share/luci/menu.d/luci-app-ipsec-vpnd.json
+#sed -i 's/"IPSec VPN Server"/"IPSec VPN 服务器"/g' feeds/luci/applications/luci-app-ipsec-vpnd/root/usr/share/luci/menu.d/luci-app-ipsec-vpnd.json
 sed -i 's/"Tailscale"/"TailScale虚拟网络"/g' package/waynesg/luci-app-tailscale/root/usr/share/luci/menu.d/luci-app-tailscale.json
 TIME b "重命名 完成"
 
