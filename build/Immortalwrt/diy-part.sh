72631c3c4e620ee9456289bcce3e705d3617142e
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-05-19 17:56:44 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -145,11 +145,11 @@ sed -i 's/"挂载点"/"挂载路径"/g' feeds/luci/modules/luci-base/po/zh_Hans/
 sed -i 's/"启动项"/"启动管理"/g' feeds/luci/modules/luci-base/po/zh_Hans/base.po
 sed -i 's/"软件包"/"软件管理"/g' feeds/luci/modules/luci-base/po/zh_Hans/base.po
 sed -i 's/"终端"/"命令终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
-sed -i 's/"Argon 主题设置"/"主题设置"/g' package/waynesg/luci-theme-argon/luci-app-argon-config/po/zh_Hans/argon-config.po
+sed -i 's/"Argon 主题设置"/"主题设置"/g' package/waynesg/luci-theme-argon/po/zh_Hans/argon-config.po
 
 #Argon主题修改
-sed -i 's/(<%= ver.luciversion %>)//g' package/waynesg/luci-theme-argon/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-sed -i 's/<%= ver.distversion %>/Compiled By @waynesg/g' package/waynesg/luci-theme-argon/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+sed -i 's/(<%= ver.luciversion %>)//g' package/waynesg/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+sed -i 's/<%= ver.distversion %>/Compiled By @waynesg/g' package/waynesg/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
 echo "重命名服务菜单"
 
 #services menu
