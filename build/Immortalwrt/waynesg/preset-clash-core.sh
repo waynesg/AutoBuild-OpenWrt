dc814d4a4ed70eee80a6ebaa870f9785181e0ca4
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-05-19 17:48:12 +0800

    Update preset-clash-core.sh

--- build/Immortalwrt/waynesg/preset-clash-core.sh
+++ build/Immortalwrt/waynesg/preset-clash-core.sh
@@ -16,14 +16,13 @@ mkdir -p files/usr/share/openclash/ui/metacubexd
 mkdir -p files/usr/share/openclash/ui/zashboard
 
 
-#CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
-#CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-#CORE_TUN_VER=$(curl -sL $CORE_VER | sed -n "2{s/\r$//;p;q}")
-
+CORE_VERSION_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
+CLASH_META_VER=$(curl -sL $CORE_VERSION_URL | sed -n '3p')
+echo ">>> 当前 Meta 核心版本：$CLASH_META_VER"
 #CLASH_DEV_URL="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
 #CLASH_TUN_URL="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
 
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/meta/clash-linux-amd64.tar.gz"
+CLASH_META_URL="https://github.com/vernesong/OpenClash/releases/download/mihomo/clash-linux-amd64.tar.gz"
 
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
