c439c5bcd29e301ab9616b34b98e3debc0c4029a
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-06-01 18:44:39 +0800

    Update diy-repo-script.sh

--- build/Immortalwrt/waynesg/diy-repo-script.sh
+++ build/Immortalwrt/waynesg/diy-repo-script.sh
@@ -39,8 +39,7 @@ clone_or_update_git_repo() {
 
 # theme
 git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/waynesg/luci-theme-argon
-git clone --depth=1 -b master https://github.com/thinktip/luci-theme-neobird.git
-git clone --depth=1 -b master https://github.com/emxiong/luci-theme-design
+git clone --depth=1 -b main https://github.com/thinktip/luci-theme-neobird.git
 # argon-theme-config
 git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/waynesg/luci-app-argon-config
 #git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon package/waynesg/luci-theme-argon
