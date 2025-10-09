7b1f6ebc1a35a6e797f2612e15a204a6f28c3277
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-10-09 14:58:35 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -82,22 +82,22 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 # ############################################################################################################################################################
 # ############################################################################################################################################################
 # echo
-TIME b "汉化 调整..."
-sed -i 's/CPU Load/处理器负载/g' package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
-rm -rf package/waynesg/luci-app-cpu-status/po/zh_Hans/cpu-status.po
-wget -O package/waynesg/luci-app-cpu-status/po/zh_Hans/cpu-status.po https://raw.githubusercontent.com/waynesg/scripts/main/others/cpu-status.po
-sed -i "s/'user %'/'用户 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
-sed -i "s/'nice %'/'调整优先级 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
-sed -i "s/'system %'/'系统 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
-sed -i "s/'idle %'/'空闲 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
-sed -i "s/'iowait %'/'IO 等待 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
-sed -i "s/'irq %'/'硬件中断 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
-sed -i "s/'softirq %'/'软中断 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# TIME b "汉化 调整..."
+# sed -i 's/CPU Load/处理器负载/g' package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# rm -rf package/waynesg/luci-app-cpu-status/po/zh_Hans/cpu-status.po
+# wget -O package/waynesg/luci-app-cpu-status/po/zh_Hans/cpu-status.po https://raw.githubusercontent.com/waynesg/scripts/main/others/cpu-status.po
+# sed -i "s/'user %'/'用户 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# sed -i "s/'nice %'/'调整优先级 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# sed -i "s/'system %'/'系统 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# sed -i "s/'idle %'/'空闲 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# sed -i "s/'iowait %'/'IO 等待 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# sed -i "s/'irq %'/'硬件中断 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
+# sed -i "s/'softirq %'/'软中断 %'/g" package/waynesg/luci-app-cpu-status/htdocs/luci-static/resources/view/status/include/18_cpu.js
 
 #tn-netports调整
-sed -i '/var title = E.*netports-title/,/);/c\var title = E('"'"'div'"'"', { class: '"'"'netports-title'"'"' }, [\n\t\t\t\tE('"'"'div'"'"', { class: '"'"'netports-buttons'"'"' }, buttons),\n\t\t\t\tE('"'"'div'"'"', { class: '"'"'netports-version'"'"' })\n\t\t\t]);' package/waynesg/luci-app-tn-netports/htdocs/luci-static/resources/netports.js
+# sed -i '/var title = E.*netports-title/,/);/c\var title = E('"'"'div'"'"', { class: '"'"'netports-title'"'"' }, [\n\t\t\t\tE('"'"'div'"'"', { class: '"'"'netports-buttons'"'"' }, buttons),\n\t\t\t\tE('"'"'div'"'"', { class: '"'"'netports-version'"'"' })\n\t\t\t]);' package/waynesg/luci-app-tn-netports/htdocs/luci-static/resources/netports.js
 #删除首页端口状态
-mv feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js.del
+# mv feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js.del
 
 echo
 TIME b "菜单 调整..."
