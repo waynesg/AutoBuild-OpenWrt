35c5aebb93423ba62e0d89238941ad126d0cffb7
claw <claw@local>
2026-03-24 04:32:30 +0000

    fix: use /tmp for AutoUpdate backup path

--- build/common/AutoUpdate.sh
+++ build/common/AutoUpdate.sh
@@ -81,11 +81,6 @@ opkg list | awk '{print $1}' > ${Download_Path}/Installed_PKG_List
 export PKG_List="${Download_Path}/Installed_PKG_List"
 export AutoUpdate_Log_Path="/tmp"
 export BACKDIR="/tmp"
-if [ -d /mnt/sda1 ] && grep -qs " /mnt/sda1 " /proc/mounts; then
-	export BACKDIR="/mnt/sda1"
-elif [ -d /mnt ] && grep -qs " /mnt " /proc/mounts; then
-	export BACKDIR="/mnt"
-fi
 [ ! -d "${BACKDIR}" ] && mkdir -p "${BACKDIR}"
 GET_PID() {
 	local Result
