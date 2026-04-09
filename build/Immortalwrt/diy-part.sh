89346692de5e3a5d433d4fb37ab10de10ba7c55f
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2026-04-09 10:14:19 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -84,7 +84,7 @@ echo
 TIME b "菜单 调整..."
 sed -i 's|/services/|/network/|' feeds/luci/applications/luci-app-3cat/root/usr/share/luci/menu.d/luci-app-3cat.json
 sed -i 's|/system/|/nas/|' feeds/luci/applications/luci-app-filemanager/root/usr/share/luci/menu.d/luci-app-filemanager.json
-sed -i 's|/services/|/vpn/|' package/waynesg/luci-app-tailscale-community/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
+sed -i 's|/services/|/vpn/|' feeds/luci/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
 sed -i 's|/services/|/control/|' feeds/luci/applications/luci-app-wol/root/usr/share/luci/menu.d/luci-app-wol.json
 sed -i 's/\"services\"/\"control\"/g'  package/waynesg/luci-app-oaf/luci-app-oaf/luasrc/controller/appfilter.lua
 #sed -i 's|/services/|/network/|' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
@@ -156,11 +156,11 @@ echo "重命名VPN菜单"
 sed -i 's/"ZeroTier"/"ZeroTier虚拟网络"/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
 sed -i 's/"OpenVPN"/"OpenVPN 客户端"/g' feeds/luci/applications/luci-app-openvpn/luasrc/controller/openvpn.lua
 #sed -i 's/"IPSec VPN Server"/"IPSec VPN 服务器"/g' feeds/luci/applications/luci-app-ipsec-vpnd/root/usr/share/luci/menu.d/luci-app-ipsec-vpnd.json
-sed -i 's/"Tailscale"/"TailScale虚拟网络"/g' package/waynesg/luci-app-tailscale-community/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
+sed -i 's/"Tailscale"/"TailScale虚拟网络"/g' feeds/luci/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
 
 TIME y "调整 VPN 菜单顺序"
 sed -i 's/"order": 90,/"order": 10,/' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
-sed -i 's/"order": 90,/"order": 20,/' package/waynesg/luci-app-tailscale-community/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
+sed -i 's/"order": 90,/"order": 20,/' feeds/luci/luci-app-tailscale-community/root/usr/share/luci/menu.d/luci-app-tailscale-community.json
 
 TIME y "ZeroTier 启动延迟 10 秒"
 ZT_INIT_PATH="$(find feeds package -path '*/etc/init.d/zerotier' 2>/dev/null | head -n 1)"
