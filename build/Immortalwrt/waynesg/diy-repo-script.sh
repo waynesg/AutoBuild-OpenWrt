9b02f5cf5ca91426118781ddb0cd4c666f72d612
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2026-05-18 20:32:53 +0800

    Update diy-repo-script.sh

--- build/Immortalwrt/waynesg/diy-repo-script.sh
+++ build/Immortalwrt/waynesg/diy-repo-script.sh
@@ -112,6 +112,4 @@ git checkout 9e433a2
 #git clone --depth=1 https://github.com/timsaya/openwrt-bandix package/waynesg/openwrt-bandix
 #rm -f "package/waynesg/openwrt-bandix/.github/workflows/Auto compile with openwrt sdk.yml"
 # cups-web
-git clone --depth=1 https://github.com/waynesg/luci-app-cups-web /tmp/luci-app-cups-web
-rm -rf feeds/luci/applications/luci-app-cups-web
-mv /tmp/luci-app-cups-web feeds/luci/applications/luci-app-cups-web
+git clone --depth=1 https://github.com/waynesg/luci-app-cups-web package/waynesg/luci-app-cups-web
